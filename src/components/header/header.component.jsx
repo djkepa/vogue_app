8452d597857e59d9718b344becb7c30d4a09a3ba
djkepa <deejaykepa@gmail.com>
2020-07-24 22:11:01 +0200

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import {ReactComponent as Logo} from '../../assets/logo.svg'
+import { auth } from '../../firebase/firebase.utils';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo"/>
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
-    </div>
-  </div>
-)
+const Header = ({ currentUser }) => (
+	<div className="header">
+		<Link to="/" className="logo-container">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link className="option" to="/shop">
+				SHOP
+			</Link>
+			<Link className="option" to="/contact">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					{' '}
+					SIGN OUT{' '}
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					{' '}
+					SIGN IN{' '}
+				</Link>
+			)}
+		</div>
+	</div>
+);
 
 export default Header;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyD0_OelJpLEW6J0nFC3n81hSXroY93iUc4",
+  authDomain: "vogue-app.firebaseapp.com",
+  databaseURL: "https://vogue-app.firebaseio.com",
+  projectId: "vogue-app",
+  storageBucket: "vogue-app.appspot.com",
+  messagingSenderId: "915584661211",
+  appId: "1:915584661211:web:a8c409a77c7dd40ac8fefe",
+  measurementId: "G-5LZZ3KW0Y2"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters( {prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
