974b21452a432f354f570e98d414154bbed89744
djkepa <deejaykepa@gmail.com>
2020-07-26 12:23:45 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,18 +4,12 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className="preview">
-      {
-        items
-        .filter((item, idx) => idx < 4)
-        .map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
-        ))}
-    </div>
-  </div>
-
-)
+const CollectionPreview = ({ title, items }) => (
+	<div className="collection-preview">
+		<h1 className="title">{title.toUpperCase()}</h1>
+		<div className="preview">
+			{items.filter((item, idx) => idx < 4).map((item) => <CollectionItem key={item.id} item={item} />)}
+		</div>
+	</div>
+);
 export default CollectionPreview;
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItem: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
+      }
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItem: addItemToCart(state.cartItem, action.payload)
       }
     default: 
       return state;
